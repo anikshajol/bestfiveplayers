d244fa58c93f08cb65c28e9429543c7d679263d7
anikshajol <anikshajol@gmail.com>
2022-08-22 02:59:42 +0600

    javascript added for budget section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,83 @@
-console.log('hi');
+
+
+// player-expenses
+
+function playerExpense(elementId, element){
+    const getPlayerExpenses = document.getElementById(elementId);
+
+    
+     
+
+    getPlayerExpenses.innerText = element;
+
+}
+
+
+
+
+// budget section
+
+document.getElementById('calculate').addEventListener('click', function () {
+
+
+    // perPlayer
+
+    const getPerPlayerCost = document.getElementById('per-player');
+
+    const perPlayerCost = parseInt(getPerPlayerCost.value);
+
+
+
+   
+    //player expenses
+
+
+
+    playerExpense("player-expenses", perPlayerCost)
+
+   
+   
+
+
+})
+
+
+
+
+
+//calculate total cost
+
+
+function getValueById(inputId){
+    const getAnotherCost = document.getElementById(inputId);
+
+    const anotherCost = parseInt(getAnotherCost.value);
+
+    return anotherCost;
+}
+
+
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+   const managerCost = getValueById('manager-cost');
+   const coachCost = getValueById('coach-cost');
+
+   const subTotal = managerCost+coachCost;
+
+   const getPlayerExpense = document.getElementById('player-expenses');
+
+   const playerExpense = parseInt(getPlayerExpense.innerText);
+
+ 
+
+   const totalCost = subTotal+playerExpense;
+
+   const getTotalExpenseOfMyTeam = document.getElementById('total-expenses');
+
+
+
+   getTotalExpenseOfMyTeam.innerText = totalCost;
+
+})
+
+
